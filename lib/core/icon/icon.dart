c8ff26df4d70edf8f873a4d108ba8593a962062f
MuhammadAli <mukhammadali@gmail.com>
2025-04-04 23:04:02 +0500

    MuhammadAli_test_versiyada

---
+++ lib/core/icon/icon.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+sealed class AppIcon{
+  static const back=Icon(Icons.arrow_back_rounded);
+  static const home=Icon(Icons.home_outlined);
+  static const center=Icon(Icons.business);
+  static const person=Icon(Icons.account_circle_outlined);
+}
---
+++ lib/core/widget/fluttew_widget.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+sealed class FlutterIcon{
+  static const flutterIcon=FlutterLogo();
+}
